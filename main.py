dc21dcd5072989fe24839c95fbd3284162cff417
Cyrus C <cyrus.castco@gmail.com>
2024-01-19 12:00:48 +0800

    move modules

--- main.py
+++
@@ -1,3 +0,0 @@
-from sudoku_variants import Sudoku, SudokuAI, SudokuConst
-from sudoku_variants.func import board as B, rules as R
-from sudoku_variants.rule import Consecutive, Jigsaw, King, Knight, Orthogonal, SubBoard
